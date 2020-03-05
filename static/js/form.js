919a6955da9a8261a2d7051ed81cc75fc3e70b81
puglord96 <55310319+puglord96@users.noreply.github.com>
2020-03-05 14:56:54 +0800

    added line functionality

--- static/js/form.js
+++ static/js/form.js
@@ -1,39 +1,71 @@
-
 //For the MapBox API functionality
 mapboxgl.accessToken = 'pk.eyJ1IjoianNvbWEiLCJhIjoibFJmYl9JWSJ9.AUm8d76cbOvVEn2mMeG_ZA';
-  var map = new mapboxgl.Map({
+var map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/mapbox/streets-v9',
-    center:[midArray[1],midArray[0]],
-    zoom : 15
-  });
-  map.on('load', function() {
+    center: [midArray[1], midArray[0]],
+    zoom: 15
+});
+
+map.on('load', function () {
     var geojsonData = {
-      "type": "FeatureCollection",
-      "features": [
-      {
-        "type": "Feature",
-        "geometry": {
-          "type": "Point",
-          "coordinates": [103.9072,1.3984]
-        }
-      },
-      {
-        "type": "Feature",
-        "geometry": {
-          "type": "Point",
-          "coordinates": [103.8984397,1.4090686]
-        }
-      }
-      ]
+        "type": "FeatureCollection",
+        "features": [
+            {
+                "type": "Feature",
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [103.9072, 1.3984]
+                }
+            },
+            {
+                "type": "Feature",
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [103.8984397, 1.4090686]
+                }
+            },
+
+        ]
     };
+
+    map.addSource('route', {
+        'type': 'geojson',
+        'data': {
+            'type': 'Feature',
+            'properties': {},
+            'geometry': {
+                'type': 'LineString',
+                'coordinates': [
+                    [103.9072, 1.3984],
+                    [103.8984397, 1.4090686]
+                ]
+            }
+        }
+    });
     map.addLayer({
-      "id": "points",
-      "type": "circle",
-      "source": {
-        "type": "geojson",
-        "data": geojsonData
-      }
-    })
-  });
+        "id": "points",
+        "type": "circle",
+        "source": {
+            "type": "geojson",
+            "data": geojsonData
+        }
+    });
+
+    map.addLayer({
+        'id': 'route',
+        'type': 'line',
+        'source': 'route',
+        'layout': {
+            'line-join': 'round',
+            'line-cap': 'round'
+        },
+        'paint': {
+            'line-color': '#888',
+            'line-width': 4
+        }
+    });
+
+
+});
 
