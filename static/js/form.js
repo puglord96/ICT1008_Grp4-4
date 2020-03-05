c8d8008e8c15b9b5d656a18436b38a63347da367
puglord96 <55310319+puglord96@users.noreply.github.com>
2020-03-05 14:10:25 +0800

    added functionality to centralise map with chosen mrt location

--- static/js/form.js
+++ static/js/form.js
@@ -4,7 +4,7 @@ mapboxgl.accessToken = 'pk.eyJ1IjoianNvbWEiLCJhIjoibFJmYl9JWSJ9.AUm8d76cbOvVEn2m
   var map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/mapbox/streets-v9',
-    center:[103.9072,1.3984],
+    center:[midArray[1],midArray[0]],
     zoom : 15
   });
   map.on('load', function() {
